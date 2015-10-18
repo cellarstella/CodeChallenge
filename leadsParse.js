ae2d65c8b58317fe31377aec9aee5d5a34e6e19b
Laura Pelofske <laura.cellarstella@gmail.com>
2015-10-18 13:11:36 -0700

    added some style

--- leadsParse.js
+++ leadsParse.js
@@ -19,22 +19,38 @@ request.onreadystatechange = function() {
 // If either of these loops returns a number greater than -1, the record will not be added.
 // Since the array is already sorted by date, the first object encountered with a later duplicate id will be the most recent.
     var uniqueLeads = dateSortedLeads.slice()
-			  .reduce(function (a,b) {
-				  	function indexOfProperty (a, b){
-				  	for (var i=0; i<a.length; i++){
-				  		if(a[i]._id === b._id || a[i].email === b.email){  // if this record id matches OR email matches...
-				  			return i;              // return the index which is 0 or more
-				  		}
-				  	}
-				  	return -1; //if nothing has been returned, it's not a match
-				  }
-				  console.log(a, b, indexOfProperty(a,b));
-			  	if (indexOfProperty(a,b) <= 0) a.push(b);
-			  	return a;
-			}, []);
-			  for(i=0; i<uniqueLeads.length; i++){
-					document.getElementById('leads').innerHTML += uniqueLeads[i]._id + ' ' + ' ' + uniqueLeads[i].email + ' ' + 
-					uniqueLeads[i].firstName + ' ' + uniqueLeads[i].lastName + ' ' + uniqueLeads[i].entryDate + '</br>';
+		  .reduce(function (a,b) {
+			  	function indexOfProperty (a, b){
+			  	for (var i=0; i<a.length; i++){
+			  		if(a[i]._id === b._id || a[i].email === b.email){  // if this record id matches OR email matches...
+			  			return i;              // return the index which is 0 or more
+			  		}
+			  	}
+			  	return -1; //if nothing has been returned, it's not a match
 			  }
+			  console.log(a, b, indexOfProperty(a,b));
+		  	if (indexOfProperty(a,b) <= 0) a.push(b);
+		  	return a;
+			}, []);
+		function dateFormat(value) {
+	    var a;
+	    if (typeof value === 'string') {
+	    	// 2008-01-01T12:00:00Z
+	    	// 2014-05-07T17:30:20+00:00
+	    		var re = /^(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2})\+(\d{2}):(\d{2})?$/;
+	        a = re.exec(value);
+	        if (a) {
+	                return new Date(Date.UTC(+a[1], +a[2] - 1, +a[3], +a[4], +a[5], +a[6]));
+	            }
+	        }
+        return value;
+    };
+	  for (i = 0; i < uniqueLeads.length; i++) {
+      document.getElementById('leads').innerHTML +=
+      '<ul class="contact"><li class="name">' + uniqueLeads[i].firstName + ' ' + uniqueLeads[i].lastName + 
+      '<span class="id">' + uniqueLeads[i]._id + '</span></li>' +
+      '<li class="dateAdded">' + dateFormat(uniqueLeads[i].entryDate).toUTCString() + '</li>' +
+      '<li class="email">' + uniqueLeads[i].email + '</li></ul>';
+		}
   } 
 };
